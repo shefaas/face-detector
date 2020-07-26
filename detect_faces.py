de5e60310a67cd8431afda476e245b2592ef2a8f
Shefaa <shefaasomar@gmail.com>
2020-07-27 01:55:35 +0300

    Add detect_faces.py and model files

---
+++ detect_faces.py
@@ -0,0 +1,79 @@
+# Detecting faces on images and video streams using OpenCV and deep learning.
+# Resources: https://www.pyimagesearch.com/2018/02/26/face-detection-with-opencv-and-deep-learning/
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+
+def argParsing():
+	# construct the argument parse and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True, help="path to input image")
+	ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
+	ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
+	ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
+
+	return vars(ap.parse_args())
+
+
+def imagePreprocessing(image):
+	# get the height and width, ignore no. of channels.
+	(h, w) = image.shape[:2]
+
+	resizedImage = cv2.resize(image, (300, 300))
+	scaleFactor = 1.0
+	size = (300, 300)
+	# Mean can be a 3-tuple of the RGB means or they can be a single value
+	mean = (104.0, 177.0, 123.0)
+
+	blob = cv2.dnn.blobFromImage(resizedImage, scaleFactor, size, mean)
+
+	return blob
+
+
+def drawBoxes(detections, args, image):
+	(h, w) = image.shape[:2]
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the prediction
+		confidence = detections[0, 0, i, 2]
+		# filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for the object
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			startX, startY, endX, endY = box.astype("int")
+
+			# draw the bounding box of the face along with the associated probability
+			text = "{:.2f}%".format(confidence * 100)
+			y = startY - 10 if startY - 10 > 10 else startY + 10
+			cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+			cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+
+args = argParsing()
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# load the input image and construct an input blob for the image
+# by resizing to a fixed 300x300 pixels and then normalizing it
+image = cv2.imread(args["image"])
+
+blob = imagePreprocessing(image)
+
+# pass the blob through the network and obtain the detections and predictions
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+
+print(detections)
+
+drawBoxes(detections, args, image)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
