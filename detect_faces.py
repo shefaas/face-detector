cd344b24c050ad37fa088a41aaef89e32426d020
Shefaa <shefaasomar@gmail.com>
2020-08-04 19:25:05 +0300

    Add cropFace and galleryImage functions

--- detect_faces.py
+++ detect_faces.py
@@ -37,10 +37,12 @@ def imagePreprocessing(image):
 
 def drawBoxes(detections, args, image):
 	(h, w) = image.shape[:2]
+
 	# loop over the detections
 	for i in range(0, detections.shape[2]):
 		# extract the confidence (i.e., probability) associated with the prediction
 		confidence = detections[0, 0, i, 2]
+
 		# filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
 		if confidence > args["confidence"]:
 			# compute the (x, y)-coordinates of the bounding box for the object
@@ -52,6 +54,45 @@ def drawBoxes(detections, args, image):
 			y = startY - 10 if startY - 10 > 10 else startY + 10
 			cv2.rectangle(image, (startX, startY), (endX, endY), (50, 0, 200), 2)
 			cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (50, 0, 200), 2)
+
+
+def cropFace(detections, args, image):
+	croppedFaces = []
+	(h, w) = image.shape[:2]
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for the object
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			startX, startY, endX, endY = box.astype("int")
+
+			croppedImage = image[startY:endY, startX:endX]
+			croppedFaces.append(croppedImage)
+
+	return croppedFaces
+
+
+def galleryImage(croppedFaces):
+
+	# assuming croppedFaces has an even number of images and the one image size
+	galleryImageWidth = len(croppedFaces) * croppedFaces[0].shape[:2][0]
+	galleryImageHeight = len(croppedFaces) * croppedFaces[0].shape[:2][1]
+	galleryImageSize = galleryImageWidth, galleryImageHeight, 3
+	galleryImage = np.zeros(galleryImageSize, dtype=np.uint8)
+
+	for i in range(len(croppedFaces)):
+		row = 0
+		col = 0
+		image = croppedFaces[i]
+
+		# TODO adding faces into galleryImage
+
+	return galleryImage
 
 
 args = argParsing()
@@ -74,9 +115,13 @@ detections = net.forward()
 
 #print(detections)
 
-resizedImage = cv2.resize(image, (int(image.shape[:2][1]/4), int(image.shape[:2][0]/4)))
-drawBoxes(detections, args, resizedImage)
+resizedImage = cv2.resize(image, (int(image.shape[:2][1]/2), int(image.shape[:2][0]/2)))
+#drawBoxes(detections, args, resizedImage)
+
+croppedFaces = cropFace(detections, args, image)
+
+galleryImage = galleryImage(croppedFaces)
 
 # show the output image
-cv2.imshow("Output", resizedImage)
+cv2.imshow("Gallery Image", galleryImage)
 cv2.waitKey(0)
